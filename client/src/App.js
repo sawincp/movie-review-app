6db0a623ae85f4217fed667bcafaea8eb345e709
Corey Sawin <sawincp@gmail.com>
2023-11-10 14:28:12 -0500

    Updated index.js with BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,14 +11,12 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
     <div className="App">
       <Routes>
         <Route exact path= '/testing' element={<h1>Test Route</h1>}></Route>
       <Route exact path="/" element={<h1>Page Count: {count}</h1>}></Route>
       </Routes>
     </div>
-    </BrowserRouter>
   );
 }
 
